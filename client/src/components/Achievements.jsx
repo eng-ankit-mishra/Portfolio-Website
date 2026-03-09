cedde5b9ba41baa59fca4afc76eb11e1c6f5a14b
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-09 21:19:28 +0530

    updated github numbers

--- client/src/components/Achievements.jsx
+++ client/src/components/Achievements.jsx
@@ -45,7 +45,7 @@ export default function AchievementsSection() {
     {
       title: (
         <span>
-          <CountUp enableScrollSpy end={1200} duration={5} />+
+          <CountUp enableScrollSpy end={1500} duration={5} />+
         </span>
       ),
       subtitle: "GitHub Contributions (2025)",
